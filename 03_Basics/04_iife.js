82ce81c1ec3005030e9cec6b5f7450b016465b6c
priyankamca10 <priyankamca2025_10@ncrdsims.edu.in>
2026-07-12 15:32:08 +0000

    iife

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,13 @@
+//Immediately invoked function expression 
+
+// Global scope ke pollution se bachne ke liye we use IIFE 
+
+(function chai(){                     // ()() here we use one bracket to wrap function inside of it and another to call it
+    console.log(`DB Connected`);
+    
+})();             //to invoke another function we use ;
+
+( (name) => {
+console.log(`DB Two Connected ${name}`);
+
+})("priya")
