bd7756250e980594907e742c0c91d83a1278c04c
priyankamca10 <priyankamca2025_10@ncrdsims.edu.in>
2026-07-17 15:45:14 +0000

    if-else if done

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -6,4 +6,36 @@ if (2 === "2"){
     
 }
 
+//global scope 
+const score ="200"
+if(score > 100){
+    const power ="fly"
+    console.log(`user power : ${power}`);
+}
+//console.log(`user power : ${power}`)
+
+
+//if else
+
+const temperature = 30
+if(temperature === 20){
+   console.log("Temperature is equal to 20");
+}
+else{
+    console.log("Temperture is not equal to 20");
+}
 // < , > , <= , >= , == , === 
+
+//if else if
+
+let balance = 1000
+
+if(balance < 500){
+    console.log("Balance is Less than 500");
+} else if(balance < 750){
+    console.log("Balance is Less than 750");
+}else if(balance < 900){
+    console.log("Balance is Less than 900");
+}else{
+    console.log("Balance is Less than 1200");
+}
