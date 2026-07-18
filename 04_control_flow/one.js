4f8c9e4400e5c293ad88e8c0b9a02550f9d81d88
priyankamca10 <priyankamca2025_10@ncrdsims.edu.in>
2026-07-18 15:53:32 +0000

    switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+// if 
+
+const isUserLoggedIn = true
+if (2 === "2"){
+    console.log("executed");
+    
+}
+
+//global scope 
+const score ="200"
+if(score > 100){
+    const power ="fly"
+    console.log(`user power : ${power}`);
+}
+//console.log(`user power : ${power}`)
+
+
+//if else
+
+const temperature = 30
+if(temperature === 20){
+   console.log("Temperature is equal to 20");
+}
+else{
+    console.log("Temperture is not equal to 20");
+}
+// < , > , <= , >= , == , === 
+
+//if else if
+
+let balance = 1000
+
+if(balance < 500){
+    console.log("Balance is Less than 500");
+} else if(balance < 750){
+    console.log("Balance is Less than 750");
+}else if(balance < 900){
+    console.log("Balance is Less than 900");
+}else{
+    console.log("Balance is Less than 1200");
+}
+
+// && (AND), || (or) operator
+
+const UserLoggedIn = true
+const debitCard = true
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(UserLoggedIn && debitCard ){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User Logged In");
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,14 @@
+const month = 3
+
+switch (month) {
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default: 
+    console.log("May");
+        break;
+}
