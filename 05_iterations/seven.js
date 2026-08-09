2a792a687ce921763e44eb8fb998360b5d6725c1
priyankamca10 <priyankamca2025_10@ncrdsims.edu.in>
2026-08-09 07:13:00 +0000

    chaining of methods

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -4,3 +4,10 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 const newNums = myNums.map( (num) => num+10)
 console.log(newNums);
 
+//Chaining 
+const newNums1 = myNums
+    .map( (num) => num * 10)
+    .map( (num) => num + 1 )    // the result of above method is pass to second method as input num
+    .filter( (num) => num >= 40) // filter is based on return of  true & false
+
+console.log(newNums1);
